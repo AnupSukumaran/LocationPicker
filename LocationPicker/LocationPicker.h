80123547c4f756a828e44d19423d178f2897c0b2
Anup Sukumaran <anup.sukumaran9@gmail.com>
2019-09-26 14:42:39 +0530

    Initial Commit

---
+++ LocationPicker/LocationPicker.h
@@ -0,0 +1,19 @@
+//
+//  LocationPicker.h
+//  LocationPicker
+//
+//  Created by Qaptive Technologies on 26/09/19.
+//  Copyright © 2019 Qaptive Technologies. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for LocationPicker.
+FOUNDATION_EXPORT double LocationPickerVersionNumber;
+
+//! Project version string for LocationPicker.
+FOUNDATION_EXPORT const unsigned char LocationPickerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <LocationPicker/PublicHeader.h>
+
+
